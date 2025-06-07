49db2283da947b1bda382695293921bff4d37690
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-06-07 19:28:16 +0530

    add functions for the game

---
+++ Stone-Paper-game/script.js
@@ -0,0 +1,7 @@
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('result');
+
