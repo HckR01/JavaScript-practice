c04b91e6a0abcaa51c33b43e1fd2da96f290b78f
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-06-13 19:03:43 +0530

    update pratice

---
+++ pratice-I/letVarConst.js
@@ -0,0 +1,16 @@
+// here we can discuss about let, var and const
+// const use for constant values
+// var use for global scope
+// let use for block scope 
+// but in general we use let and const
+
+const id =1234;
+let name = "John"; // block scope
+var age = 30; // global scope
+
+var age=34; // reassigning var is allowed
+name = "Doe"; // reassigning let is allowed
+
+console.log("ID:", id);
+console.log("Name:", name);
+console.log("Age:", age);
