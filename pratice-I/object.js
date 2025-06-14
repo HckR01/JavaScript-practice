2777b7cc0a6862f85125913d3804a286ad930dd8
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-06-14 17:09:08 +0530

    object methods

--- pratice-I/object.js
+++ pratice-I/object.js
@@ -24,3 +24,26 @@ console.log(person["email id"]); // access properties using square bracket notat
 //change valu of object properties
 person.age = 31; // change age property
 person.city = "Los Angeles"; // change city property
+
+
+let person2 = {
+      name: "Jane",
+      greet: function() {
+            console.log(`Hello, my name is ${this.name}`); // using this to access object property
+      }
+}
+//call function inside object
+person2.greet(); // Hello, my name is Jane
+
+
+//construct object using constructor function
+function Person(name, age) {
+      this.name = name; // using this to refer to the current object
+      this.age = age;
+
+}
+//using sprade operator
+
+let obj1={...person,...person2}
+console.log(obj1)
+
