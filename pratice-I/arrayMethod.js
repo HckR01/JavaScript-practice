b9b5788db4c8ceb884dd72296996660f56353418
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-07-06 11:59:32 +0530

    pratice array

--- pratice-I/arrayMethod.js
+++ pratice-I/arrayMethod.js
@@ -1,1 +1,18 @@
-//maps
+//push pop
+
+const hobbi= ['reading','song']
+console.log(hobbi)
+
+hobbi.push('coding')
+console.log(hobbi)
+
+//adding at first 
+hobbi.unshift('singing')
+console.log(hobbi)
+
+//removing from last    
+hobbi.pop()
+console.log(hobbi)
+//removing from first
+hobbi.shift()
+console.log(hobbi)
